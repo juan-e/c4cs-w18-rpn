8f4e9fa7555b8b51dfb126a85531e53c8c83680d
joncv <joncv@umich.edu>
2018-02-23 10:55:08 -0500

    added mult and division

--- rpn.py
+++ rpn.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+import operator
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
 
 def calculate(arg):
     stack = list()
@@ -7,15 +16,16 @@ def calculate(arg):
             value = int(token)
             stack.append(value)
         except ValueError:
-            if token == '+':
-                 arg1 = stack.pop()
-                 arg2 = stack.pop()
-                 return(arg1 + arg2)
-            elif token == '-':
-                 arg2 = stack.pop()
-                 arg1 = stack.pop()
-                 return(arg1 - arg2)
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError
 
+    return stack.pop()
 def main():
     while True:
        print(calculate(input("rmp calc> ")))
--- test_rpn.py
+++ test_rpn.py
@@ -9,4 +9,12 @@ class TestBasics(unittest.TestCase):
     def test_subtract(self):
         result = rpn.calculate('5 2 -')
         self.assertEqual(3, result)
+    
+    def test_multiply(self):
+        result = rpn.calculate('9 2 *')
+        self.assertEqual(18, result)
+    
+    def test_divide(self):
+        result = rpn.calculate('9 3 /')
+        self.assertEqual(3, result)
 
