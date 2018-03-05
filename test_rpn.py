e881fb10949cf0d1184360b16eb9fb6866fd9881
joncv <joncv@umich.edu>
2018-03-05 15:37:25 -0500

    Adding some coveralls support

--- test_rpn.py
+++ test_rpn.py
@@ -1,6 +1,11 @@
 import unittest
 import rpn
 
+require 'coveralls'
+Coveralls.wear!
+
+# Your code here.
+
 class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate('1 1 +')
